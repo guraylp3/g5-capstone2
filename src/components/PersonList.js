470c53693c6cb5df80eca3705d15a19750872329
EC2 Default User <ec2-user@ip-172-31-52-51.us-west-2.compute.internal>
2024-02-06 21:45:40 +0000

    test

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -17,12 +17,9 @@ export default class PersonList extends React.Component {
   render() {
     return (
       <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.id}>{person.id}{person.name}</li>
-            )
-        }
+        hello
+        
+        
       </ul>
     )
   }
