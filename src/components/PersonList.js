0724eec930bef2b48258e91956d178b16d2443e6
Training <training@ip-172-31-24-114.us-east-2.compute.internal>
2024-02-08 22:37:33 +0000

    create env variable for app api id

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://d7kg9y2s0j.execute-api.us-west-2.amazonaws.com/tf_prod/get-person`)
+    axios.get(`https://${process.env.REACT_APP_API_ID}.execute-api.us-west-2.amazonaws.com/tf_prod/get-person`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
